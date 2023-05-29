29851cee956486071cadcdbae6d8edcfd039778e
seanr89 <srafferty89@gmail.com>
2023-05-29 11:35:47 +0100

    added health checks!

--- src/services/boulders/boulder.api/Program.cs
+++ src/services/boulders/boulder.api/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using HealthChecks.UI.Client;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,8 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHealthChecks()
 .AddCheck<SampleHealthCheck>(
         "Sample",
-        failureStatus: HealthStatus.Degraded,
-        tags: new[] { "sample" });
+        failureStatus: HealthStatus.Degraded);
+
+// Registers required services for health checks
+builder.Services.AddHealthChecksUI(setupSettings: setup =>
+{
+    setup.SetEvaluationTimeInSeconds(15); // Configures the UI to poll for healthchecks updates every 5 seconds
+}
+).AddInMemoryStorage();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -16,7 +24,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.MapHealthChecks("/healthz");
+app.MapHealthChecks("/health", new HealthCheckOptions()
+{
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -28,6 +40,18 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 // app.UseAuthorization();
+
+      // {
+      //   "Name": "Ordering HTTP Check",
+      //   "Uri": "http://host.docker.internal:5102/hc"
+      // },
+      // {
+      //   "Name": "Ordering HTTP Background Check",
+      //   "Uri": "http://host.docker.internal:5111/hc"
+      // }
+
+// build the app, register other middleware
+app.UseHealthChecksUI(config => config.UIPath = "/hc-ui");
 
 app.MapControllers();
 
