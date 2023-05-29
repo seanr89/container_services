43ef28ddc73c682fb8f3c0c007c43995066e5cc8
seanr89 <srafferty89@gmail.com>
2023-05-29 21:10:14 +0100

    got a build script to work

---
+++ src/scripts/docker_build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e -u #Exit immediately if a command exits with a non-zero status.
+
+echo "Executing script!"
+
+cd ../services/boulders/boulder.api
+docker build . -f Dockerfile -t 'boulderapi:latest'
+
+cd ../../
+
+cd ../services/webhooks/webhooks.api
+docker build . -f Dockerfile -t 'webhooks:latest'
+
+echo "Dockerisation Complete!"
