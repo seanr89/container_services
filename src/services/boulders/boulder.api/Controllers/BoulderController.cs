a1b3cacf0fa32a6e44017828f27a641baa296e07
seanr89 <srafferty89@gmail.com>
2023-06-13 23:04:14 +0100

    refresh items

--- src/services/boulders/boulder.api/Controllers/BoulderController.cs
+++ src/services/boulders/boulder.api/Controllers/BoulderController.cs
@@ -6,26 +6,19 @@ namespace boulder.api.Controllers;
 [Route("[controller]")]
 public class BoulderController : ControllerBase
 {
-    // private readonly ILogger<BoulderController> _logger;
-    // private readonly BoulderService _boulderService;
+    private readonly ILogger<BoulderController> _logger;
+    private readonly BoulderService _boulderService;
 
-    // public BoulderController(ILogger<BoulderController> logger,
-    //                          BoulderService boulderService)
-    // {
-    //     _logger = logger;
-    //     _boulderService = boulderService;
-    // }
+    public BoulderController(ILogger<BoulderController> logger,
+                             BoulderService boulderService)
+    {
+        _logger = logger;
+        _boulderService = boulderService;
+    }
 
     [HttpGet(Name = "GetBoulders")]
     public IEnumerable<Boulder> Get()
     {
-        throw new NotImplementedException();
-        // return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        // {
-        //     Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-        //     TemperatureC = Random.Shared.Next(-20, 55),
-        //     Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        // })
-        // .ToArray();
+        return _boulderService.GetAllBoulders();
     }
 }
--- src/services/boulders/boulder.api/Services/BoulderService.cs
+++ src/services/boulders/boulder.api/Services/BoulderService.cs
@@ -1,8 +1,19 @@
 
 public class BoulderService
 {
-    public BoulderService()
+    internal readonly BoulderContext _context;
+    public BoulderService(BoulderContext context)
     {
-        
+        _context = context;
+    }
+
+    public int GetBoulderCount()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Boulder> GetAllBoulders()
+    {
+        throw new NotImplementedException();
     }
 }
