ff3902dd6eda9924ba2668161013d8ed7afa1a4a
seanr89 <srafferty89@gmail.com>
2023-07-11 07:29:44 +0100

    active flags included

--- src/services/boulders/boulder.api/Models/Boulder.cs
+++ src/services/boulders/boulder.api/Models/Boulder.cs
@@ -3,8 +3,16 @@ public record Boulder
 {
     public Guid Id { get; private set; }
     public string Name { get; set; }
-    //public string ContactEmail { get; set; }
     public bool Active { get; set; } = false;
     public DateTime ActiveDate { get; set; }
+    public DateTime DeActiveDate { get; set; }
     public Grouping BoulderGroup { get; set; }
+
+    #region Public Methods
+
+    public void Activate () => this.Active = true;
+
+    public void Deactivate () => this.Active = false;   
+
+    #endregion
 }
--- src/services/boulders/boulder.api/Models/Location.cs
+++ src/services/boulders/boulder.api/Models/Location.cs
@@ -11,6 +11,8 @@ public class Location : AuditableEntity
     public bool Private { get; set; }
     public List<Grouping> BoulderGroups { get; set; }
 
+    #region Constructors
+
     public Location(string name, bool active, bool @private)
     {
         this.Name = name;
@@ -22,4 +24,19 @@ public class Location : AuditableEntity
     {
         this.Url = url;
     }
+
+    #endregion
+
+    #region Public Methods
+
+    public void AddBoulderGroup(Grouping boulderGroup)
+    {
+        this.BoulderGroups.Add(boulderGroup);
+    }
+
+    public void Activate () => this.Active = true;
+
+    public void Deactivate () => this.Active = false;   
+
+    #endregion
 }
