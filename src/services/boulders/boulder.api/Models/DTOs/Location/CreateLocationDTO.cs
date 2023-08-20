c1f455191664d5d2dbb22e7729ecb6cc6c069dee
seanr89 <srafferty89@gmail.com>
2023-08-20 20:33:42 +0100

    small edits

--- src/services/boulders/boulder.api/Models/DTOs/Location/CreateLocationDTO.cs
+++ src/services/boulders/boulder.api/Models/DTOs/Location/CreateLocationDTO.cs
@@ -2,11 +2,9 @@
 public class CreateLocationDTO
 {
     public string Name { get; set; }
+    public bool Active { get; set; }
     public bool IsPrivate { get; set; }
-
-    public Location ToLocation(CreateLocationDTO dto) => new Location
-    {
-        Name = dto.Name,
-        IsPrivate = dto.IsPrivate
-    };
+    public Location ToLocation(CreateLocationDTO dto) {
+        return new Location(dto.Name, dto.Active, dto.IsPrivate);
+    }
 }
